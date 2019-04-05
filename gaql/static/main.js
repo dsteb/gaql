12c2cd8638d49e2d028206e19c6627aa3648852b
Dmitrii Stebliuk <dmitrii.stebliuk@goeuro.com>
2019-04-05 18:14:51 +0200

    Introduce pseudo pagination
    I didn't find the way to paginate with Python Google Ads Api Library,
    so the pagination is done on the client side (with pandas).

--- gaql/static/main.js
+++ gaql/static/main.js
@@ -1,5 +1,6 @@
 (function() {
-  window.onload = function() {
+
+  function initCodeMirror() {
     const sqlTextArea = document.getElementById('query');
     const mime = 'text/x-mariadb';
     CodeMirror.fromTextArea(sqlTextArea, {
@@ -12,5 +13,47 @@
       extraKeys: {"Ctrl-Space": "autocomplete"},
       hintOptions: {}
     });
+  }
+
+  function navigateToPage(page) {
+    document.getElementById('page').value = page.toString();
+    const form = document.getElementById('query-form');
+    form.submit();
+  }
+
+  function initPagination() {
+    const elements = document.querySelectorAll('.page-link');
+
+    if (elements.length) {
+
+      let active = -1;
+      for (let i = 1; i < elements.length - 1; ++i) {
+        const el = elements[i];
+
+        if (!el.parentElement.className.includes('active')) {
+          el.onclick = function() {
+            const page = parseInt(this.innerText);
+            navigateToPage(page);
+          }
+        } else {
+          active = i;
+        }
+      }
+      prev = elements[0];
+      if (!prev.parentElement.className.includes('disabled')) {
+        const prevPage = parseInt(elements[active - 1].innerText);
+        prev.onclick = function() { navigateToPage(prevPage); };
+      }
+      next = elements[elements.length - 1];
+      if (!next.parentElement.className.includes('disabled')) {
+        const nextPage = parseInt(elements[active + 1].innerText);
+        next.onclick = function() { navigateToPage(nextPage); };
+      }
+    }
+  }
+
+  window.onload = function() {
+    initCodeMirror();
+    initPagination();
   };
 })();
--- gaql/views.py
+++ gaql/views.py
@@ -1,15 +1,16 @@
 """
     HTTP handlers for Query
 """
-from flask import (
-    Blueprint, render_template, request,
-)
+import math
+
+from flask import Blueprint, render_template, request
 
 from . import services
 
 bp = Blueprint('auth', __name__, url_prefix='/query')
 
-_DEFAULT_PAGE_SIZE = 1000
+
+DEFAULT_PAGE_SIZE = 50
 
 
 @bp.route('/', methods=('GET', 'POST'))
@@ -18,15 +19,54 @@ def query_view():
     if request.method == 'POST':
         query = request.form['query']
         customer_id = request.form['customer-id']
+        page = int(request.form['page'])
 
         df = services.run_query_to_df(customer_id, query)
+        total_items = len(df)
+        df = _slice(df, page, DEFAULT_PAGE_SIZE)
+        paginator = _get_pagination(df, page, DEFAULT_PAGE_SIZE, total_items)
 
         return render_template('query/view.html', table=_to_html(df),
-                               customer_id=customer_id, query=query)
+                               customer_id=customer_id, query=query,
+                               pagination=paginator)
 
     return render_template('query/view.html')
 
 
 def _to_html(df):
     return df.to_html(classes=['table', 'table-bordered', 'table-striped',
-                               'table-hover'], index=False)
+                               'table-hover'])
+
+
+def _slice(df, page, page_size):
+    start, end = _page_to_positions(page, page_size)
+    return df[start:end]
+
+
+def _page_to_positions(page, page_size):
+    start = page_size * (page - 1)
+    return start, start + page_size
+
+
+def _get_pagination(df, page, page_size, total_items):
+    total_pages = math.ceil(total_items / page_size)
+    visible = len(df) > 0
+    if total_pages <= 1:
+        visible = False
+        pages = []
+    elif total_pages == 2:
+        pages = [1, 2]
+    elif page <= 1:
+        pages = [1, 2, 3]
+    elif page >= total_pages:
+        pages = [total_pages - 2, total_pages - 1, total_pages]
+    else:
+        pages = [page - 1, page, page + 1]
+
+    return {
+        'visible': visible,
+        'has_previous': page > 1,
+        'has_next': page < total_pages,
+        'page': page,
+        'pages': pages
+    }
