1008f31896d0fcc8d8e520f6427d4c2ad40984d4
Dmitrii Stebliuk <dmitrii.stebliuk@goeuro.com>
2019-04-24 10:03:49 +0200

    Prefix templates

--- gaql/__init__.py
+++ gaql/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask, redirect, jsonify, url_for
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
@@ -18,16 +17,20 @@ def create_app(test_config=None):
         pass
 
     from . import views
-    app.register_blueprint(views.bp)
 
-    @app.route('/')
-    def root():
-        return redirect(url_for('query.query_view'))
+    app.register_blueprint(views.bp)
 
     @app.route('/health')
     def health():
         resp = jsonify(success=True)
         resp.status_code = 200
         return resp
+
+    @app.context_processor
+    def prefix_paths():
+        path_prefix = os.getenv('PATH_PREFIX')
+        if path_prefix:
+            return dict(path_prefix=path_prefix)
+        return dict()
 
     return app
